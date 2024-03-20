3e9d9caf7ffd09d47b0cbb43d3ce448cc10f7a5b
akihiro <aponmaru@gmail.com>
2024-03-20 17:02:33 +0900

    add: Item の マイグレーションとモデル、シーダー作成

---
+++ database/migrations/2024_03_20_074712_create_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable()->comment('商品名');
+            $table->integer('price')->nullable()->comment('価格');
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
