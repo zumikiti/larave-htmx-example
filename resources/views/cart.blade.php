6f8ed3a8659ae84cefec4b0cf385202dcbcb277e
akihiro <aponmaru@gmail.com>
2024-03-20 16:37:58 +0900

    modify: カートレイアウト調整

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,6 +1,8 @@
-<div>
+<div class="bg-slate-100 dark:bg-slate-800 p-3">
+    <div>カート</div>
+
     @foreach ($items as $item)
-        <div class="w-auto bg-slate-100 rounded-xl dark:bg-slate-800 m-3 p-3">
+        <div class="w-auto rounded-xl m-3 p-3 border-2 border-sky-500">
             <div>商品名：{{ $item['name'] }}</div>
             <div>金額：{{ $item['price'] }}</div>
             <button
