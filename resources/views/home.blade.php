08e783bfb1794fb017ce00c638e134712ce75df4
akihiro <aponmaru@gmail.com>
2024-03-20 14:56:28 +0900

    add: hx-get でカートをサイドに表示

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,15 @@
 @section('title', 'home')
 
 @section('main')
-    home
+    <div class="md:flex">
+        <div class="mx-auto">
+            home
+        </div>
+        <div
+            hx-get="/cart"
+            hx-trigger="load"
+            class="md:w-48 md:h-auto"
+            >
+        </div>
+    </div>
 @endsection
