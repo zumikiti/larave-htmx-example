9481df4b6ff8b781d105bfa1487632eae0ea43c9
akihiro <aponmaru@gmail.com>
2024-06-23 08:54:08 +0900

    update: htmx 2 に変更

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -14,7 +14,7 @@
         <script src="https://cdn.tailwindcss.com"></script>
 
         <!-- htmx -->
-        <script src="https://unpkg.com/htmx.org@1.9.11" integrity="sha384-0gxUXCCR8yv9FM2b+U3FDbsKthCI66oH5IA9fHppQq9DDMHuMauqq1ZHBpJxQ0J0" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/htmx.org@2.0.0/dist/htmx.min.js"></script>
     </head>
 
     <body class="font-sans antialiased">
